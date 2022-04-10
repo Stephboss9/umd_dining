271939140c61d382dd7a63e6185a2c75663e4bd5
Stephboss9 <mbengastephane34@gmail.com>
2022-04-10 00:12:34 -0400

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'UMD Dining Menu',
+      title: 'UMD Dining senu',
       theme: ThemeData(
           // This is the theme of your application.
           //
